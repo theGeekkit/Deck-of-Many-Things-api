34b519122b7e7d33012e1865afa41573eba7daa0
theGeekkit <sonofgondolin16@gmail.com>
2023-05-24 17:30:52 -0500

    5-24-23
    Co-authored-by: chrisHembree <chrisHembree@users.noreply.github.com>
    Co-authored-by: Joe Lambert <jLamwuzhere@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+cards = [
+    { title: "Balance", info: "Change alignment instantly.", image: "https://i.postimg.cc/ncskz8yY/Balance.jpg", used: false},
+    { title: "Comet", info: "Defeat the next monster you meet to gain one level.", image: "https://i.postimg.cc/8CGBt7jt/Comet.jpg", used: false},
+    { title: "DonJon", info: "You are imprisoned.", image: "https://i.postimg.cc/4dL6fwwS/DonJon.jpg", used: false},
+    { title: "Eurayle", info: "–1 penalty on all saving throws henceforth.", image: "https://i.postimg.cc/wMnDJvC5/Euryale.jpg", used: false},
+    { title: "The Fates", info: "Avoid any situation you choose, once.", image: "https://i.postimg.cc/kXtQCQST/Fates.jpg", used: false},
+    { title: "Flames", info: "Enmity between you and an outsider.", image: "https://i.postimg.cc/9f1TbJ3Q/Flames.jpg", used: false},
+    { title: "Fool", info: "Lose 10,000 experience points and you must draw again.", image: "https://i.postimg.cc/s1FZxLWw/Fool.jpg", used: false},
+    { title: "Gem", info: "Gain your choice of 25 pieces of jewelry or 50 gems.", image: "https://i.postimg.cc/nVNdqcYw/Gem.jpg", used: false},
+    { title: "Idiot", info: "Lose 1d4+1 Intelligence. You may draw again.", image: "https://i.postimg.cc/0y0cgGgL/Idiot.jpg", used: false},
+    { title: "Jester", info: "Gain 10,000 XP or two more draws from the deck.", image: "https://i.postimg.cc/bvYLqGpd/Jester.jpg", used: false},
+    { title: "Key", info: "Gain a major magic weapon.", image: "https://i.postimg.cc/7hznK0Mt/Key.jpg", used: false},
+    { title: "Knight", info: "Gain the service of a 4th-level fighter.", image: "https://i.postimg.cc/W186Y2gV/knight.jpg", used: false},
+    { title: "Moon", info: "You are granted 1d4 wishes.", image: "https://i.postimg.cc/LXYtsL74/Moon.jpg", used: false},
+    { title: "Rogue", info: "One of your friends turns against you.", image: "https://i.postimg.cc/02qY86Cx/Rogue.jpg", used: false},
+    { title: "Ruin", info: "Immediately lose all wealth and property.", image: "https://i.postimg.cc/6pMnLMx5/Ruin.jpg", used: false},
+    { title: "Skull", info: "Defeat dread wraith or be forever destroyed.", image: "https://i.postimg.cc/gjy6wDKp/skull.jpg", used: false},
+    { title: "Star", info: "Immediately gain a +2 inherent bonus to one ability score.", image: "https://i.postimg.cc/bwVS2d1Z/Star.jpg", used: false},
+    { title: "Sun", info: "Gain beneficial medium wondrous item and 50,000 XP.", image: "https://i.postimg.cc/FR3YqZ8S/Sun.jpg", used: false},
+    { title: "Talons", info: "All magic items you possess disappear permanently.", image: "https://i.postimg.cc/YqbhJjnd/Talon.jpg", used: false},
+    { title: "Throne", info: "Gain a +6 bonus on Diplomacy checks plus a small castle.", image: "https://i.postimg.cc/6q1xhscf/Throne.jpg", used: false},
+    { title: "Vizier", info: "Know the answer to your next dilemma.", image: "https://i.postimg.cc/SRRwRT42/Vizier.jpg", used: false},
+    { title: "The Void", info: "Body functions, but soul is trapped elsewhere.", image: "https://i.postimg.cc/wTtCdGFn/Void.jpg", used: false}
+  ]
+
+cards.each do |card_info|
+    Card.create(card_info)
+end
